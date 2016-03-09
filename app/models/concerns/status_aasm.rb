1f4e95d4c48f4e8b1a7a41194a2783a61af5a63e
Emil Shakirov <5o.smoker@gmail.com>
2016-03-09 09:36:28 +0300

    introduce aasm

---
+++ app/models/concerns/status_aasm.rb
@@ -0,0 +1,39 @@
+module StatusAasm
+  extend ActiveSupport::Concern
+  included do
+    include AASM
+
+    aasm column: :status, enum: true do
+      state :pending, initial: true
+      state :paid
+      state :confirmed
+      state :partpaid
+      state :overpaid
+      state :timeout
+
+      event :pay do
+        transitions from: :pending, to: :paid
+      end
+
+      event :overpay do
+        transitions from: %i(pending partpaid), to: :overpaid
+      end
+
+      event :partpay do
+        transitions from: :pending, to: :partpaid
+      end
+
+      event :confirm do
+        transitions from: :paid, to: :confirmed
+      end
+
+      event :expire do
+        transitions from: %i(pending paid partpaid overpaid), to: :timeout
+      end
+
+      event :requote do
+        transitions from: :timeout, to: :pending
+      end
+    end
+  end
+end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,4 +9,16 @@ describe Invoice do
     it { is_expected.to validate_presence_of(:price_cents) }
     it { is_expected.to validate_presence_of(:currency) }
   end
+
+  describe "StatusAASM" do
+    let(:subject) { described_class.new }
+
+    it { is_expected.to have_state(:pending) }
+    it { is_expected.to transition_from(:pending).to(:paid).on_event(:pay) }
+    it { is_expected.to transition_from(*%i(pending partpaid)).to(:overpaid).on_event(:overpay) }
+    it { is_expected.to transition_from(:pending).to(:partpaid).on_event(:partpay) }
+    it { is_expected.to transition_from(:paid).to(:confirmed).on_event(:confirm) }
+    it { is_expected.to transition_from(*%i(pending paid partpaid overpaid)).to(:timeout).on_event(:expire) }
+    it { is_expected.to transition_from(:timeout).to(:pending).on_event(:requote) }
+  end
 end
